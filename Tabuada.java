fb05a09cdbe5d93bdb7418864b2fdcd330a21cc1
joao2314 <119130483+joao2314@users.noreply.github.com>
2023-12-21 17:57:05 -0300

    Add files via upload

---
+++ Tabuada.java
@@ -0,0 +1,18 @@
+
+import java.util.Scanner;
+import java.util.Locale;
+
+public class Tabuada {
+
+    public static void main(String[] args) {
+        System.out.print("Digite a tabuda que voce deseja ver: ");
+        Scanner scan = new Scanner(System.in);
+        int tabuada = scan.nextInt();
+        
+        int limit = 0;
+        while(limit <= 10) {
+            System.out.println(tabuada + " x " + limit + " = " + tabuada * limit);
+            limit++;
+        }
+        }
+    }
